797a9d18a0d21a7248c574e464a905d5d4aa7e6c
robert.traghetto <Robert.Traghetto@take2games.com>
2020-01-22 16:37:04 -0500

    1

--- container/sagemaker_runner.py
+++ container/sagemaker_runner.py
@@ -5,11 +5,11 @@ bucket = 't2-edw-dev-sagemaker'
 project = 'sagemaker-decision-trees'
 import sagemaker as sage
 from pathlib import Path
+
 print(Path(__file__).parent)
 import sys
 
 sess = sage.Session()
-
 
 WORK_DIRECTORY = str(Path(__file__).parent.parent) + '/data'
 data_location = sess.upload_data(WORK_DIRECTORY, key_prefix=prefix)
@@ -22,23 +22,25 @@ print(account)
 print(region)
 print(image)
 
-
 print(sys.argv)
-
 
 model_output_path = "s3://{}/{}/{}/{}/".format(bucket, project, sys.argv[2], sys.argv[1])
 
-
 tree = sage.estimator.Estimator(image,
-                       role, 1, 'ml.c4.2xlarge',
-                       output_path= model_output_path, sagemaker_session=sess)
+                                role, 1, 'ml.c4.2xlarge',
+                                output_path=model_output_path,
+                                sagemaker_session=sess,
+                                metric_definitions=[
+                                    {'Name': 'train:error', 'Regex': 'Train_error=(.*?);'},
+                                    {'Name': 'validation:error', 'Regex': 'Valid_error=(.*?);'}
+                                ]
+                                )
 
 tree.fit(data_location, wait=True, logs="All")
 
-
+print('start transformer')
 
 transform_output_folder = "batch-transform-output"
-
 
 transformer = tree.transformer(instance_count=1,
                                instance_type='ml.m4.xlarge',
@@ -46,8 +48,5 @@ transformer = tree.transformer(instance_count=1,
                                assemble_with='Line',
                                accept='text/csv')
 
-
 transformer.transform(data_location, content_type='text/csv', split_type='Line', input_filter='$[1:]')
 transformer.wait()
-
-
