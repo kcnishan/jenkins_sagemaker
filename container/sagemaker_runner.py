abb82285ffa2dd5ba7b8a5ceb67bff7fb5764f0c
robert.traghetto <Robert.Traghetto@take2games.com>
2020-01-22 10:25:13 -0500

    1

--- container/sagemaker_runner.py
+++ container/sagemaker_runner.py
@@ -2,6 +2,7 @@
 role = 'arn:aws:iam::084602632950:role/t2-edw-dev-sagemaker'
 prefix = 'DEMO-scikit-byo-iris'
 bucket = 't2-edw-dev-sagemaker'
+project = 'sagemaker-decision-trees'
 import sagemaker as sage
 from pathlib import Path
 print(Path(__file__).parent)
@@ -15,7 +16,7 @@ data_location = sess.upload_data(WORK_DIRECTORY, key_prefix=prefix)
 
 account = sess.boto_session.client('sts').get_caller_identity()['Account']
 region = sess.boto_session.region_name
-image = '{}.dkr.ecr.{}.amazonaws.com/sagemaker-decision-trees:latest'.format(account, region)
+image = '{}.dkr.ecr.{}.amazonaws.com/{}:latest'.format(account, region, project)
 
 print(account)
 print(region)
@@ -25,7 +26,7 @@ print(image)
 print(sys.argv)
 tree = sage.estimator.Estimator(image,
                        role, 1, 'ml.c4.2xlarge',
-                       output_path="s3://{}/{}/{}/output".format(bucket, sys.argv[2], sys.argv[1]), sagemaker_session=sess)
+                       output_path="s3://{}/{}/{}/{}/".format(bucket, project, sys.argv[2], sys.argv[1]), sagemaker_session=sess)
 
 tree.fit(data_location, wait=False, logs="All")
 
